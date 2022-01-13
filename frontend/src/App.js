3f98186689778fb7b2a0c49196a7f36a7cdea260
camora1990 <moorales.camilo@gmail.com>
2022-01-13 17:40:53 -0500

    definiendo rutas privadas y publicas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { AdminProducts } from "./components/AdminProducts";
+import { AdminUser } from "./components/AdminUser";
 import { Login } from "./components/Login";
+import { MyProducts } from "./components/MyProducts";
+import { Products } from "./components/Products";
 import { Register } from "./components/Register";
+import { useUser } from "./context/UserContext";
 
 function App() {
+  const { user } = useUser();
+  const Public = (props) => {
+    return user.Login ? <Redirect to="/products" /> : <Route {...props} />;
+  };
+  const Private = (props) => {
+    if (user.login) {
+      if (props.path == "/admin-products" || props.path == "/admin-users") {
+        return user.isAdmin ? (
+          <Route {...props} />
+        ) : (
+          <Redirect to="/products" />
+        );
+      } else {
+        return <Route {...props} />;
+      }
+    } else {
+      return <Redirect to="/" />;
+    }
+  };
+
   return (
     <Router>
-      <Route path="/" exact component={Login} />
-      <Route path="/register" exact component={Register}/>
+      <Public path="/" exact component={Login} />
+      <Public path="/register" exact component={Register} />
+      <Private path="/products" exact component={Products} />
+      <Private path="/my-products" exact component={MyProducts} />
+      <Private path="/admin-products" exact component={AdminProducts} />
+      <Private path="/admin-users" exact component={AdminUser} />
     </Router>
   );
 }
