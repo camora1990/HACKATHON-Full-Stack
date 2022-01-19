6f3a619bf025eeb75829f4e033d150d48cc1620f
camora1990 <moorales.camilo@gmail.com>
2022-01-18 21:10:05 -0500

    se agrega spinner loading

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 import { useUser } from "../context/UserContext";
 import { useHistory, Link } from "react-router-dom";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 export const Register = () => {
-  const [loading, setLoading] = useState(false)
   const inicialUser = {
     name: "",
     email: "",
     password: "",
   };
   const history = useHistory();
-  const { registerUser } = useUser();
+  const { registerUser, loading } = useUser();
   const [user, setUser] = useState(inicialUser);
-  const register = async(e) => {
-    setLoading(true)
+  const register = async (e) => {
     e.preventDefault();
-    await registerUser(user,history)
-    setLoading(false)
+    await registerUser(user, history);
   };
   return (
     <div className="container h-100 mt-5 ">
       <div className="d-flex row justify-content-center aling-items-center h-100">
-        <div className="col-12 col-md-9 col-lg-6 col-xl-4">
+        <div className="col-12 col-md-9 col-lg-6 col-xl-4 position-relative">
           <div className="card card-custon">
+            {loading && (
+              <div className="loading-custom position-absolute  translate-middle top-50 start-0 translate-middle-y w-100">
+                <LoadingSpinner />
+              </div>
+            )}
             <div className="card-body p-4">
               <h2 className="text-uppercase text-center mb-5">Register</h2>
               <form onSubmit={register}>
@@ -43,7 +46,7 @@ export const Register = () => {
                   />
                   <label htmlFor="floatingInput">Name</label>
                 </div>
-                
+
                 <div className="form-floating mb-3">
                   <input
                     type="email"
@@ -92,18 +95,6 @@ export const Register = () => {
                 </button>
               </form>
             </div>
-            {loading && (
-              <div className="card-footer d-flex justify-content-center">
-                <button className="btn btn-primary" type="button" disabled>
-                  <span
-                    className="spinner-grow spinner-grow-sm"
-                    role="status"
-                    aria-hidden="true"
-                  ></span>
-                    wait a moment please.....
-                </button>
-              </div>
-            )}
           </div>
         </div>
       </div>
